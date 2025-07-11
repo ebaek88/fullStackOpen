747a9b94be2e010c4943ba1fabce7bcdf4e60f31
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-07-11 14:32:11 -0600

    feat: add answers for Part 2, up to Exercise 2.8
    Exercises 2.6 - 2.17 are stored in the directory
    
    part2/phonebook.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,25 +1,42 @@
 import { useState } from "react";
 
-const DisplayName = ({ name }) => {
-  return <li style={{ listStyle: "none" }}>{name}</li>;
+const DisplayLine = ({ name, number }) => {
+  return (
+    <li style={{ listStyle: "none" }}>
+      {name} {number}
+    </li>
+  );
 };
 
 const Display = ({ persons }) => {
   return (
     <ul style={{ padding: "0px" }}>
       {persons.map((person) => (
-        <DisplayName key={person.name} name={person.name} />
+        <DisplayLine
+          key={person.name}
+          name={person.name}
+          number={person.number}
+        />
       ))}
     </ul>
   );
 };
 
-const AddNewName = ({ value, onSubmit, onChange }) => {
+const AddNew = (props) => {
+  const newName = props.newName;
+  const newNumber = props.newNumber;
+  const onSubmit = props.onSubmit;
+  const onNameChange = props.onNameChange;
+  const onNumberChange = props.onNumberChange;
+
   return (
     <>
-      <form name="new-name" onSubmit={onSubmit}>
+      <form name="new-entry" onSubmit={onSubmit}>
         <div>
-          name: <input value={value} onChange={onChange} />
+          name: <input value={newName} onChange={onNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={onNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -32,22 +49,49 @@ const AddNewName = ({ value, onSubmit, onChange }) => {
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
-  const addName = (evt) => {
+  const addNew = (evt) => {
     evt.preventDefault();
-    // Checks if the newly put name already exists in the phonebook.
+    const trimmedNewName = newName.trim();
+    const trimmedNewNumber = newNumber.trim();
+    // Checks if the newly put entry already exists in the phonebook.
     // If so, an alert message pops up.
-    if (persons.some((person) => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`);
+    if (
+      persons.some(
+        (person) =>
+          person.name === trimmedNewName && person.number === trimmedNewNumber
+      )
+    ) {
+      alert(
+        `The entry ${trimmedNewName} ${trimmedNewNumber} is already added to phonebook`
+      );
       return;
     }
-    const personObject = {
-      name: newName,
-    };
 
-    // console.log("event target: ", evt.target);
-    setPersons(persons.concat(personObject));
+    const personObject = {
+      name: trimmedNewName,
+      number: trimmedNewNumber,
+    };
+    let newPersons = [...persons];
+
+    // If the newly put entry's name is already existing in the phonebook but the number is not
+    if (persons.some((person) => person.name === trimmedNewName)) {
+      newPersons.splice(
+        newPersons.findIndex((person) => person.name === trimmedNewName),
+        1,
+        personObject
+      );
+    } else {
+      // If the entry is a brand new one
+      newPersons = newPersons.concat(personObject);
+    }
+
+    // console.log("Current persons: ", persons);
+    // console.log("New persons: ", newPersons);
+    setPersons(newPersons);
     setNewName("");
+    setNewNumber("");
   };
 
   const handleNameChange = (evt) => {
@@ -55,15 +99,20 @@ const App = () => {
     setNewName(evt.target.value);
   };
 
-  // console.log(persons);
+  const handleNumberChange = (evt) => {
+    // console.log(evt.target.value);
+    setNewNumber(evt.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <AddNewName
-        value={newName}
-        onChange={handleNameChange}
-        onSubmit={addName}
+      <AddNew
+        newName={newName}
+        newNumber={newNumber}
+        onNameChange={handleNameChange}
+        onSubmit={addNew}
+        onNumberChange={handleNumberChange}
       />
       <h2>Numbers</h2>
       <Display persons={persons} />
