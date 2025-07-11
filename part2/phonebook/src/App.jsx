5b2cfc5fde3fea7a1261fd25cbbc4f2ded3b0d61
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-07-11 15:24:57 -0600

    feat: add answers for Part 2, up to Exercise 2.9
    Exercises 2.6 - 2.17 are stored in the directory
    
    part2/phonebook.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,19 @@
 import { useState } from "react";
 
+const SearchBar = ({ filterText, onChange }) => {
+  return (
+    <div>
+      <label htmlFor="searchbar">filter shown with</label>
+      <input
+        type="text"
+        value={filterText}
+        onChange={onChange}
+        id="searchbar"
+      />
+    </div>
+  );
+};
+
 const DisplayLine = ({ name, number }) => {
   return (
     <li style={{ listStyle: "none" }}>
@@ -8,18 +22,21 @@ const DisplayLine = ({ name, number }) => {
   );
 };
 
-const Display = ({ persons }) => {
-  return (
-    <ul style={{ padding: "0px" }}>
-      {persons.map((person) => (
-        <DisplayLine
-          key={person.name}
-          name={person.name}
-          number={person.number}
-        />
-      ))}
-    </ul>
-  );
+const Display = ({ persons, filterText }) => {
+  const displayLines = [];
+  persons.forEach((person) => {
+    if (
+      person.name.indexOf(filterText) === -1 &&
+      person.number.indexOf(filterText) === -1
+    ) {
+      return;
+    }
+    displayLines.push(
+      <DisplayLine key={person.id} name={person.name} number={person.number} />
+    );
+  });
+  // console.log(displayLines);
+  return <ul style={{ padding: "0px" }}>{displayLines}</ul>;
 };
 
 const AddNew = (props) => {
@@ -33,10 +50,11 @@ const AddNew = (props) => {
     <>
       <form name="new-entry" onSubmit={onSubmit}>
         <div>
-          name: <input value={newName} onChange={onNameChange} />
+          name: <input id="name" value={newName} onChange={onNameChange} />
         </div>
         <div>
-          number: <input value={newNumber} onChange={onNumberChange} />
+          number:{" "}
+          <input id="number" value={newNumber} onChange={onNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -47,9 +65,15 @@ const AddNew = (props) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filterText, setFilterText] = useState("");
 
   const addNew = (evt) => {
     evt.preventDefault();
@@ -76,14 +100,19 @@ const App = () => {
     let newPersons = [...persons];
 
     // If the newly put entry's name is already existing in the phonebook but the number is not
+    // , copy the existing number and id into a new entry.
     if (persons.some((person) => person.name === trimmedNewName)) {
+      personObject.id = persons.find(
+        (person) => person.name === trimmedNewName
+      ).id;
       newPersons.splice(
         newPersons.findIndex((person) => person.name === trimmedNewName),
         1,
         personObject
       );
     } else {
-      // If the entry is a brand new one
+      // If the entry is a brand new one, create a new entry.
+      personObject.id = persons.length + 1;
       newPersons = newPersons.concat(personObject);
     }
 
@@ -104,9 +133,16 @@ const App = () => {
     setNewNumber(evt.target.value);
   };
 
+  const handleFilterTextChange = (evt) => {
+    // console.log(evt.target.value);
+    setFilterText(evt.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <SearchBar filterText={filterText} onChange={handleFilterTextChange} />
+      <h2>add a new</h2>
       <AddNew
         newName={newName}
         newNumber={newNumber}
@@ -115,7 +151,7 @@ const App = () => {
         onNumberChange={handleNumberChange}
       />
       <h2>Numbers</h2>
-      <Display persons={persons} />
+      <Display persons={persons} filterText={filterText} />
     </div>
   );
 };
