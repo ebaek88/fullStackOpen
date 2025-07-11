60cee2ddbfc2a9bd403e3ae07f2aa67cf710831d
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-07-11 12:57:35 -0600

    feat: add answers for Part 2, up to Exercise 2.6
    Exercises 2.6 - 2.17 are stored in the directory
    
    part2/phonebook.

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+
+const DisplayName = ({ name }) => {
+  return <li style={{ listStyle: "none" }}>{name}</li>;
+};
+
+const Display = ({ persons }) => {
+  return (
+    <ul style={{ padding: "0px" }}>
+      {persons.map((person) => (
+        <DisplayName key={person.name} name={person.name} />
+      ))}
+    </ul>
+  );
+};
+
+const AddNewName = ({ value, onSubmit, onChange }) => {
+  return (
+    <>
+      <form name="new-name" onSubmit={onSubmit}>
+        <div>
+          name: <input value={value} onChange={onChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+    </>
+  );
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addName = (evt) => {
+    evt.preventDefault();
+    const personObject = {
+      name: newName,
+    };
+
+    // console.log("event target: ", evt.target);
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
+
+  const handleNameChange = (evt) => {
+    // console.log(evt.target.value);
+    setNewName(evt.target.value);
+  };
+
+  // console.log(persons);
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <AddNewName
+        value={newName}
+        onChange={handleNameChange}
+        onSubmit={addName}
+      />
+      <h2>Numbers</h2>
+      <Display persons={persons} />
+    </div>
+  );
+};
+
+export default App;
