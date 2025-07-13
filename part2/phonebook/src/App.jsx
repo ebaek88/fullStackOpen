74a51c3044f3974cb2e22904b27e1fc667981d5b
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-07-13 09:55:51 -0600

    feat: add answers for Part 2, up to Exercise 2.11
    Exercises 2.6 - 2.17 are stored in the directory
    
    part2/phonebook.
    
    Added dependencies axios and json-server(dev-only).
    
    Also, created db.json so that the App component fetches
    
    data from db.json and display it.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,20 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Filter from "./components/Filter.jsx";
 import Persons from "./components/Persons.jsx";
 import PersonForm from "./components/PersonForm.jsx";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
-
   /** States */
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterText, setFilterText] = useState("");
+
+  /** useEffect */
+  useEffect(() => {
+    // console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      // console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
 
   /** Event handlers */
   const addNew = (evt) => {
