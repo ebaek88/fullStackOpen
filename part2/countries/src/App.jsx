765c9c686cc8f9712bb7c992641f84a2287cfca9
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-07-15 15:27:26 -0600

    feat: add answers for Part 2, up to Exercise 2.18
    Exercises 2.18 - 2.20 are stored in the directory
    
    part2/countries.

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -9,39 +9,103 @@ const SearchBar = ({ onChange }) => {
   );
 };
 
-const ResultRow = () => {
-  return <li style={{ listStyle: "none" }}></li>;
+const ResultRow = ({ name }) => {
+  return <li style={{ listStyle: "none" }}>{name}</li>;
 };
 
-const Result = ({ results }) => {
-  return <ul>{}</ul>;
+const CountryInfo = ({ country }) => {
+  const languages = [];
+  for (const language in country.languages) {
+    languages.push(<li>{country.languages[language]}</li>);
+  }
+
+  return (
+    <>
+      <h1>{country.name.common}</h1>
+      <div>
+        <p>Capital: {country.capital}</p>
+        <p>Area(in km^2): {country.area}</p>
+      </div>
+      <h2>Languages</h2>
+      <ul>{languages}</ul>
+      <img src={country.flags.png} alt={country.flags.alt} />
+    </>
+  );
+};
+
+const Result = ({ countries }) => {
+  if (countries && countries.length > 10) {
+    return <p>Too many matches, specify another filter</p>;
+  } else if (countries && countries.length > 1) {
+    return (
+      <ul>
+        {countries.map((country) => (
+          <ResultRow key={country.cca3} name={country.name.common} />
+        ))}
+      </ul>
+    );
+  } else if (countries && countries.length === 1) {
+    // If only one country is found.
+    const country = countries[0];
+    return <CountryInfo country={country} />;
+  } else {
+    return <p>No countries found</p>;
+  }
 };
 
 const App = () => {
   /** states */
   const [value, setValue] = useState("");
+  const [queries, setQueries] = useState(null);
   const [countries, setCountries] = useState(null);
+
+  useEffect(() => {
+    if (queries !== null) {
+      console.log("fetching country info...");
+      axios
+        .get("https://studies.cs.helsinki.fi/restcountries/api/all")
+        .then((response) => {
+          const filtered = response.data.filter((country) => {
+            if (queries[1].length > 0) {
+              return (
+                country.name.common.includes(queries[0]) ||
+                country.name.common.includes(queries[1])
+              );
+            } else {
+              return country.name.common.includes(queries[0]);
+            }
+          });
+          setCountries(filtered);
+        })
+        .catch((error) => console.log(error.message));
+    }
+  }, [queries]);
 
   const handleChange = (evt) => {
     console.log(evt.target.value);
     setValue(evt.target.value);
-    query(value);
+    // query(value); -> since the update of the state(setValue) is async
+    // , use evt.target.value for the argument of query fn
+    query(evt.target.value);
   };
 
-  const query = async (value) => {
-    const response = await axios.get(
-      "https://studies.cs.helsinki.fi/restcountries/api/all"
-    );
-    const queryData = response.data.filter((country) =>
-      country.name.common.includes(value)
-    );
-    console.log(queryData);
+  const query = (value) => {
+    const beforeCapitalized = value.trim();
+    // console.log(beforeCapitalized);
+    let capitalizedQuery = "";
+    if (beforeCapitalized.length > 0) {
+      capitalizedQuery =
+        beforeCapitalized[0].toUpperCase() + beforeCapitalized.slice(1);
+      // console.log(capitalizedQuery);
+    }
+
+    setQueries([beforeCapitalized, capitalizedQuery]);
   };
 
   return (
     <div>
       <SearchBar onChange={handleChange} />
-      <Result />
+      <Result countries={countries} />
     </div>
   );
 };
