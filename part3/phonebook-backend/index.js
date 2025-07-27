1ca41bf2b7915189cf691306658e1bebda24c316
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-07-26 18:06:35 -0600

    feat: add answers for Part 3, up to Exercise 3.11
    Exercises 3.1 - 3.11 are stored in the directory
    
    part3/phonebook-backend.

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors");
+// const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+// app.use(cors({ origin: "*" }));
+app.use(express.static("dist"));
 
 morgan.token("body", (request, response) => {
   return JSON.stringify(request.body);
