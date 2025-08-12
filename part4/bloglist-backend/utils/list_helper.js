4921f4230c72eae51fe23b532f04ed866efd5fdd
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-08-12 12:11:08 -0600

    feat: add answers for Part 4, up to Exercise 4.7
    Added helper functions mostLikes and mostLikesLodash,
    
    and the unit test for them.
    
    Exercises 4.1 - 4.7 are stored in the directory
    
    part4/bloglist-backend.

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -28,4 +28,43 @@ const mostBlogs = (blogs) => {
   return mostProlificAuthor;
 };
 
-module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs };
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) return undefined;
+
+  const likesByAuthor = {};
+  const likesFromBlogs = blogs.map((blog) => [blog.author, blog.likes]);
+  likesFromBlogs.forEach((elem) => {
+    likesByAuthor[elem[0]] = (likesByAuthor[elem[0]] || 0) + elem[1];
+  });
+
+  const authorWithMostLikes = Object.entries(likesByAuthor).toSorted(
+    (a, b) => b[1] - a[1]
+  )[0];
+
+  return { author: authorWithMostLikes[0], likes: authorWithMostLikes[1] };
+};
+
+// Lodash version
+const mostLikesLodash = (blogs) => {
+  if (blogs.length === 0) return undefined;
+
+  const grouped = _.groupBy(blogs, "author");
+  // In Lodash, the callback fn for _.map has parameters in the following order:
+  // If _.map is used over arrays: (element, index)
+  // If _.map is used over object: (value, key)
+  const authorLikes = _.map(grouped, (authorBlogs, author) => ({
+    author,
+    likes: _.sumBy(authorBlogs, "likes"),
+  }));
+  const top = _.maxBy(authorLikes, "likes");
+  return top ? { author: top.author, likes: top.likes } : undefined;
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
+  mostLikesLodash,
+};
