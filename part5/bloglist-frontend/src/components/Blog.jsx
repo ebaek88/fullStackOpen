6b1d88008f853b3e8d045de5e6d567b7fb95a8ad
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-08-27 16:15:59 -0600

    feat: add answers for Part 5, up to Exercise 5.7
    Refactored the Blog component so that each entry
    
    can be toggled to display its details.
    
    Exercises 5.5 - 5.12 are stored in the directory
    
    part5/bloglist-frontend.

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author} {blog.url} {blog.likes}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisibility = () => setVisible(!visible);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  // console.log(blog.user);
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} - by {blog.author}{" "}
+        <button onClick={toggleVisibility}>{visible ? "hide" : "view"}</button>
+      </div>
+      <div style={{ display: visible ? "" : "none" }}>
+        <p>{blog.url}</p>
+        <p>
+          likes {blog.likes} <button>like</button>
+        </p>
+        {blog.user.name || " "}
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
