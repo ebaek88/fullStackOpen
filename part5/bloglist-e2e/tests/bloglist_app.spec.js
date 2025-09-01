f07750f19beba1b8563e76f05e6d8c916e1bcd93
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-08-31 19:59:09 -0600

    feat: add answers for Part 5, up to Exercise 5.17
    Created a new npm project for tests and configured
    
    Playwright there. Also made a test to ensure that
    
    the application displays the login form by default.
    
    Exercises 5.17 - 5.23 are stored in the directory
    
    part5/bloglist-e2e.

---
+++ part5/bloglist-e2e/tests/bloglist_app.spec.js
@@ -0,0 +1,16 @@
+const { test, expect, beforeEach, describe } = require("@playwright/test");
+
+describe("Blog app", () => {
+  beforeEach(async ({ page }) => {
+    await page.goto("http://localhost:5173");
+  });
+
+  test("login form is shown", async ({ page }) => {
+    await expect(page.getByText("log in to application")).toBeVisible();
+    await expect(page.getByLabel("username")).toBeVisible();
+    await expect(page.getByLabel("password")).toBeVisible();
+    await expect(
+      page.getByRole("button").filter({ hasText: "login" })
+    ).toBeVisible();
+  });
+});
