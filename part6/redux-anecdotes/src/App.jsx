8be6865fe59484e40b64924589f42bc07b5ea470
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-09-05 10:50:52 -0600

    feat: add answers for Part 6, up to Exercise 6.14
    Implemented simple backend using json-server. Also,
    
    implemented fetching the anecdotes from the backend.
    
    Exercises 6.14 - 6.19 are stored in the directory
    
    part6/redux-anecdotes.

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from "react";
 import AnecdoteForm from "./components/AnecdoteForm.jsx";
 import AnecdoteList from "./components/AnecdoteList.jsx";
 import Filter from "./components/Filter.jsx";
 import Notification from "./components/Notification.jsx";
+import anecdoteService from "./services/anecdotes.js";
+import { setNotes } from "./reducers/anecdoteReducer.js";
+import { useDispatch } from "react-redux";
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    anecdoteService.getAll().then((notes) => dispatch(setNotes(notes)));
+  }, []);
+
   return (
     <div>
       <h2>Anecdotes</h2>
