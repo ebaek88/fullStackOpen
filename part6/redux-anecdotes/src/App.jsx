56f12f49cf423b4b41317a64cf9e34bb88081ff3
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-09-05 14:08:57 -0600

    feat: add answers for Part 6, up to Exercise 6.16
    Modified the initialization of the Redux store to
    
    happen using async action creators, which are made
    
    possible by the Redux Thunk library.
    
    Exercises 6.14 - 6.19 are stored in the directory
    
    part6/redux-anecdotes.

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -3,16 +3,13 @@ import AnecdoteForm from "./components/AnecdoteForm.jsx";
 import AnecdoteList from "./components/AnecdoteList.jsx";
 import Filter from "./components/Filter.jsx";
 import Notification from "./components/Notification.jsx";
-import anecdoteService from "./services/anecdotes.js";
-import { setAnecdotes } from "./reducers/anecdoteReducer.js";
+import { initializeAnecdotes } from "./reducers/anecdoteReducer.js";
 import { useDispatch } from "react-redux";
 
 const App = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    anecdoteService
-      .getAll()
-      .then((anecdotes) => dispatch(setAnecdotes(anecdotes)));
+    dispatch(initializeAnecdotes());
   }, []);
 
   return (
