a592d01e8ee6607321ea8608c13a4bb8c35ce1b8
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-09-06 20:00:55 -0600

    feat: add answers for Part 6, up to Exercise 6.20
    Implemented retrieving anecdotes from the server
    
    using React Query. Also, implemented a simple error
    
    page, just in case the client cannot fetch the data
    
    from the server.
    
    Exercises 6.20 - 6.22 are stored in the directory
    
    part6/query-anecdotes.

---
+++ part6/query-anecdotes/src/App.jsx
@@ -0,0 +1,58 @@
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+
+import { getAnecdotes } from "./requests.js";
+import AnecdoteForm from "./components/AnecdoteForm";
+import Notification from "./components/Notification";
+
+const App = () => {
+  const result = useQuery({
+    queryKey: ["anecdotes"],
+    queryFn: getAnecdotes,
+    retry: 1,
+  });
+  console.log(JSON.parse(JSON.stringify(result)));
+
+  if (result.isLoading) {
+    return <div>loading data...</div>;
+  }
+
+  if (result.isError) {
+    return <div>anecdote service not available due to problems in server</div>;
+  }
+
+  const anecdotes = result.data;
+
+  const handleVote = (anecdote) => {
+    console.log("vote");
+  };
+
+  // const anecdotes = [
+  //   {
+  //     "content": "If it hurts, do it more often",
+  //     "id": "47145",
+  //     "votes": 0
+  //   },
+  // ]
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
