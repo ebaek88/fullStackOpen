4006a49b8d9b8e187f0264202ec6198759bec71b
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-10-03 15:17:44 -0600

    fix: remove unnecessary loggin in the console

--- part5/bloglist-frontend/src/components/Blogs.jsx
+++ part5/bloglist-frontend/src/components/Blogs.jsx
@@ -12,7 +12,7 @@ const Blogs = ({ user, ref }) => {
 		queryFn: blogService.getAll,
 		retry: 1,
 	});
-	console.log(JSON.parse(JSON.stringify(result)));
+	// console.log(JSON.parse(JSON.stringify(result)));
 
 	if (result.isLoading) {
 		return <div>loading data...</div>;
