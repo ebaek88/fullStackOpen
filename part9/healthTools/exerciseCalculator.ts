3a21e5c87b14132232404da4f5045bb2903627e0
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-10-04 11:10:33 -0600

    feat: add answers for Part 9, up to Exercise 9.2
    Wrote a function calculateExercises that calculates
    
    the average time of daily exercise hours, compares
    
    it to the target amount of daily hours and returns
    
    an object that includes various values.
    
    Exercises 9.1 - 9.3 are stored in the directory
    
    part9/healthTools.

---
+++ part9/healthTools/exerciseCalculator.ts
@@ -0,0 +1,93 @@
+interface Ratings {
+  differenceMax: number;
+  rating: number;
+  explanation: string;
+}
+
+interface Result {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const exerciseRanges: Ratings[] = [
+  {
+    differenceMax: -1.0,
+    rating: 1,
+    explanation: "you need to exercise more to reach your target",
+  },
+  {
+    differenceMax: -0.25,
+    rating: 2,
+    explanation: "not too bad but could be be better",
+  },
+  {
+    differenceMax: 0.25,
+    rating: 3,
+    explanation: "you are really doing well. keep going!",
+  },
+  {
+    differenceMax: 0.5,
+    rating: 2,
+    explanation: "not too bad but could be be better",
+  },
+];
+
+const calculateExercises = (dailyHours: number[], target: number): Result => {
+  const periodLength = dailyHours.length;
+  const trainingDays = dailyHours.filter((dailyHour) => dailyHour > 0).length;
+  const average =
+    dailyHours.reduce((acc, curr) => acc + curr, 0) / periodLength;
+  const difference = average - target;
+  const success = difference >= 0;
+  // const exerciseRating = exerciseRanges.find(
+  //   (range) => difference < range.differenceMax
+  // );
+  // const { rating, explanation: ratingDescription } = exerciseRating ?? {
+  //   rating: 1,
+  //   explanation: "you are exercising too much.",
+  // };
+  let rating = 0,
+    ratingDescription = "";
+
+  switch (true) {
+    case difference < -1.0 || difference >= 0.5:
+      rating = 1;
+      ratingDescription =
+        difference < -1.0
+          ? "you need to exercise more to reach your target"
+          : "you are exercising too much.";
+      break;
+    case (difference >= -1.0 && difference < -0.25) ||
+      (difference >= 0.25 && difference < 0.5):
+      rating = 2;
+      ratingDescription = "not too bad but could be be better";
+      break;
+    case difference >= -0.25 && difference < 0.25:
+      rating = 3;
+      ratingDescription = "you are really doing well. keep going!";
+      break;
+    default:
+      throw new Error("cannot interpret the data");
+  }
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  };
+};
+
+// console.log(calculateExercises([0, 0, 0, 4.5, 0, 0, 1], 2)); //'you need to exercise more to reach your target'
+// console.log(calculateExercises([0, 0, 1, 4.5, 0, 3, 1], 2)); //'not too bad but could be be better'
+// console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2)); //'you are really doing well. keep going!'
+// console.log(calculateExercises([3, 3, 3, 4.5, 1, 1.5, 1], 2)); //'not too bad but could be be better'
+console.log(calculateExercises([3, 3, 3, 4.5, 1.5, 1.5, 1], 2)); //'you are exercising too much.'
