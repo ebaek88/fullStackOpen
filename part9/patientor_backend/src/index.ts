f20abae87acf2b8edea060be6fae5201c78ed560
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-10-06 20:25:34 -0600

    feat: add answers for Part 9, up to Exercise 9.9
    Forked and cloned the frontend project for patientor.
    
    Also modified the backend so that it integrates with
    
    the frontend by allowing CORS for the origin of the
    
    frontend.
    
    Exercises 9.8 - 9.9 are stored in the directory
    
    part9/patientor_backend and part9/patientor_frontend.

--- part9/patientor_backend/src/index.ts
+++ part9/patientor_backend/src/index.ts
@@ -1,10 +1,18 @@
 import express = require("express");
+import cors = require("cors");
+
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // react address
+    credentials: true, // to include cookies or credentials
+  })
+);
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get("/ping", (_req, res) => {
+app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
   res.send("pong");
 });
