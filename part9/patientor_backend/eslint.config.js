70ccd56971cba5527113d877efbadbf1c1913066
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-10-06 19:44:52 -0600

    feat: add answers for Part 9, up to Exercise 9.8
    Initialized a new backend project patientor_backend.
    
    Exercises 9.8 - 9.9 are stored in the directory
    
    part9/patientor_backend.

---
+++ part9/patientor_backend/eslint.config.js
@@ -0,0 +1,33 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import stylistic from "@stylistic/eslint-plugin";
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  { ignores: ["eslint.config.js", "dist/*"] },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    plugins: {
+      "@stylistic": stylistic,
+    },
+    rules: {
+      "@stylistic/semi": "error",
+      "@typescript-eslint/no-unsafe-assignment": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/restrict-template-expressions": "off",
+      "@typescript-eslint/restrict-plus-operands": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+    },
+  }
+);
