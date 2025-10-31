2a23488ba1413ca319373c79df4ba6ed5109ea10
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-10-31 15:08:57 -0600

    fix: modify Redis initialization
    Modified the initial value of Redis to be 0 instead
    
    of the initial number of todos in Mongo DB.
    
    Also, the syntax of node-redis has been changed to
    
    cater to Redis v4, where client.get() and client.set()
    
    themselves return Promise.

--- part12-containers-applications/todo-app/todo-backend/redis/index.js
+++ part12-containers-applications/todo-app/todo-backend/redis/index.js
@@ -1,41 +1,7 @@
 const redis = require("redis");
 // const { promisify } = require("util");
+// since Redis v4, promisify is no longer needed since client.get() and client.set() themselves return Promise
 const { REDIS_URL } = require("../util/config");
-const { Todo } = require("../mongo");
-
-// let getAsync;
-// let setAsync;
-
-// if (!REDIS_URL) {
-//   const redisIsDisabled = () => {
-//     console.log("No REDIS_URL set, Redis is disabled");
-//     return null;
-//   };
-//   getAsync = redisIsDisabled;
-//   setAsync = redisIsDisabled;
-// } else {
-//   const client = redis.createClient({
-//     url: REDIS_URL,
-//   });
-
-//   client.on("connect", async () => {
-//     console.log("Connected to Redis");
-
-//     const getAsyncTemp = promisify(client.get).bind(client);
-//     const setAsyncTemp = promisify(client.set).bind(client);
-
-//     const existingValue = await getAsyncTemp("added_todos");
-//     if (existingValue === null) {
-//       const todos = await Todo.find({});
-//       await setAsyncTemp("added_todos", todos.length);
-//     }
-//   });
-
-//   client.on("error", (error) => console.error("Redis error", error));
-
-//   getAsync = promisify(client.get).bind(client);
-//   setAsync = promisify(client.set).bind(client);
-// }
 
 let client;
 
@@ -54,8 +20,7 @@ async function connectRedis() {
 
   const existingValue = await client.get("added_todos");
   if (existingValue === null) {
-    const todos = await Todo.find({});
-    await client.set("added_todos", todos.length);
+    await client.set("added_todos", 0);
   }
 
   return client;
