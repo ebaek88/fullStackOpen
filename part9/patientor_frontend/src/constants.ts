62fd474e5be682354af70e6d70659c25045a4c5e
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2026-03-27 21:18:24 -0600

    feat: add answers for Part12, up to Exercise 12.22
    Containerized development environment of
    
    part9/patientor_backend and part9/patientor_frontend.

--- part9/patientor_frontend/src/constants.ts
+++ part9/patientor_frontend/src/constants.ts
@@ -1,1 +1,2 @@
-export const apiBaseUrl = "http://localhost:3003/api";
+// export const apiBaseUrl = "http://localhost:3003/api";
+export const apiBaseUrl = "http://localhost:8080/api"; // this is for nginx reverse proxy
