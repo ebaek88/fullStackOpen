cc72be5a81a9f88e8a7e5377f26dae6b9ddc5307
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2026-04-08 02:05:46 -0600

    feat: add answers for Part13, up to Exercise 13.23
    Implemented functionality to mark a blog in the
    
    reading list as read or unread. It is done by making
    
    a request to the PUT /api/readinglists/:id path.

--- part13/blogs-backend/controllers/readinglists.js
+++ part13/blogs-backend/controllers/readinglists.js
@@ -24,6 +24,27 @@ router.post("/", tokenExtractor, userExtractor, async (req, res, next) => {
   }
 });
 
+router.put("/:id", tokenExtractor, userExtractor, async (req, res, next) => {
+  try {
+    const readingList = await ReadingList.findByPk(req.params.id);
+    if (!readingList) {
+      return res.status(404).end();
+    }
+
+    if (readingList.blogUserId !== req.user.id) {
+      return res
+        .status(401)
+        .json({ error: "the reading list can be modified only by the user" });
+    }
+
+    readingList.read = req.body.read;
+    await readingList.save();
+    res.json(readingList);
+  } catch (error) {
+    next(error);
+  }
+});
+
 router.use(errorHandler);
 
 module.exports = router;
